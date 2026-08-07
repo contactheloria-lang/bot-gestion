28e1a45eea47e4b7c69bf7d8930220d90bdd91b2
nexacorpesport-pixel <nexa.corp.esport@gmail.com>
2026-08-07 18:08:11 +0200

    Feat: Ajout complet du dossier data et des fichiers JSON/JS

---
+++ data/voiceConfig.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // Salon vocal "Rejoindre pour créer"
+    TRIGGER_CHANNEL: "1533586742668689519",
+
+    // Catégorie où seront créés les salons éphémères
+    TEMP_CATEGORY: "1533586690998800515",
+
+    // Salon de logs pour la modération vocale
+    LOGS_CHANNEL: "1533586364564508843",
+
+    // ID du Fondateur / Propriétaire principal
+    OWNER_ID: "1431661348218998948",
+
+    // IDs des rôles Staff autorisés
+    STAFF_ROLES: [
+        "1532015045800628244",
+        "1532015039806963763"
+    ]
+};
---
+++ data/welcomeConfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // ID du serveur
+    GUILD_ID: "1531784092738392144",
+
+    // Salons
+    CHANNELS: {
+        WELCOME: "1531793905073258567",        // Salon de bienvenue
+        LOGS_MEMBRES: "1533592352483049703",   // Logs des entrées / sorties
+        LOGS_INVITES: "1533592385446219896"    // Logs d'invitations détaillés
+    },
+
+    // Rôle automatique (laisser vide "" si inutilisé)
+    AUTO_ROLE_ID: "",
+
+    // URL du logo de la structure
+    LOGO_URL: "https://media.discordapp.net/attachments/1470095477344043028/1533590706751279218/087b2b1d-cbb9-427f-82ea-567a0dfb2ef1.png?ex=6a710b25&is=6a6fb9a5&hm=f26211726a5d708643abb6a39e2711a8cbfcd903da14dd65d6ba619b7a160b32&=&format=webp&quality=lossless&width=350&height=350"
+};
